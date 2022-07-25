012fdbded4fe692652dd3f1e7c70469bb867712d
Daniel Rojas <cld.rojasz@gmail.com>
2022-07-24 23:28:20 -0400

    Update vite.config.ts
    Add deploy to gh-pages base path

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,6 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	//base: '/react-todo/',
+	base: '/react-todo/',
 	plugins: [react(), tsconfigPaths()],
 });
