6bb5a25fd1ee1caa93855ef8bc98770d4069f31b
cldrojas <cld.rojasz@gmail.com>
2022-09-19 23:14:10 -0300

    Add effect for event listener handling

--- src/hocs/withStorageListener.tsx
+++ src/hocs/withStorageListener.tsx
@@ -1,13 +1,17 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 
 export function withStorageListener(WrappedComponent: any) {
 	return function WrappedComponentWithStorageListener(props: any) {
 		const [show, setShow] = useState(false);
 
-		window.addEventListener('storage', (change) => {
-			console.log('there was changes');
-			setShow(true);
-		});
+		useEffect(() => {
+			const onChange = (change: StorageEvent) => {
+				if (change.key === 'TODOS_V1') setShow(true);
+			};
+
+			window.addEventListener('storage', onChange);
+			return () => window.removeEventListener('storage', onChange);
+		}, []);
 
 		const toggleShow = () => {
 			props.syncTodos();
