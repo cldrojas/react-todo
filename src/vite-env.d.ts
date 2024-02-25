7ccf127cc50bfef3c5a43c870845b455ef3fd595
cldrojas <cld.rojasz@gmail.com>
2024-02-24 23:25:44 -0300

    Update Configs - Add no-duplicate-imports rule to eslintrc - Update prettier code style - Remove prettier-plugin-tailwind dependency - Add 'loading skeleton' animation - Add interfaces NewTodoBar, SearchButton and OpenableSearchBar

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,39 +1,53 @@
 /// <reference types="vite/client" />
 interface Todo {
-	id: number;
-	text: string;
-	done: boolean;
+	id: number
+	text: string
+	done: boolean
 }
 
 interface Counter {
-	totalTodos: number;
-	totalCompleted: number;
+	totalTodos: number
+	totalCompleted: number
 }
 
 interface SearchBar {
-	searchTerm: string;
-	setSearchTerm: React.Dispatch<React.SetStateAction<string>>;
+	searchTerm: string
+	setSearchTerm: React.Dispatch<React.SetStateAction<string>>
 }
 
 interface Form {
-	toggleModal: () => void;
-	addTodo: (text: string) => void;
+	toggleModal: () => void
+	addTodo: (text: string) => void
 }
 
 interface List {
-	children: any;
-	title: string;
-	number: number;
+	children: any
+	title: string
+	number: number
 }
 
 interface ListItem {
-	text: string;
-	done: boolean;
-	toggleComplete: () => void;
-	removeTodo: () => void;
+	text: string
+	done: boolean
+	toggleComplete: () => void
+	removeTodo: () => void
 }
 
 interface AddButton {
-	onClick: () => void;
-	isModalOpen: boolean;
+	onClick: () => void
+	isModalOpen: boolean
+}
+
+interface NewTodoBar {
+	addTodo: (text: string) => void
+}
+
+interface SearchButton {
+	onClick: () => void
+}
+
+interface OpenableSearchBar {
+	searchTerm: string
+	setSearchTerm: React.Dispatch<React.SetStateAction<string>>
+	isOpen: boolean
 }
